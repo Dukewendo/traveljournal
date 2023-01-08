c6301d9e884f75f36b9d6447438a9dc72b8419e7
Dukewendo <dommct@yahoo.co.uk>
2023-01-08 17:44:34 +0000

    style-end

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     return (
       <div>
         <Card key={place.id} {...place} />
+        <br />
       </div>
     );
   });
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,13 +4,20 @@ function Card(props) {
   return (
     <div className="place--card">
       <img className="img--url" src={props.imageUrl} alt="" />
-      <h3 className="place--location">{props.location}</h3>
-      <a href={props.googleMapsUrl} className="place--googleMaps">
-        View on Google Maps
-      </a>
+
+      <div className="place--container">
+        <h3 className="place--location">{props.location}</h3>
+        <a href={props.googleMapsUrl} className="place--googleMaps">
+          View on Google Maps
+        </a>
+      </div>
+
       <h1 className="place--title">{props.title}</h1>
-      <h3 className="place--startDate">{props.startDate}</h3>
-      <h3 className="place--endDate">- {props.endDate}</h3>
+      <div className="date--container">
+        <h3 className="place--startDate">{props.startDate}</h3>
+        <h3 className="place--endDate">- {props.endDate}</h3>
+      </div>
+
       <p className="place--description">{props.description}</p>
     </div>
   );
